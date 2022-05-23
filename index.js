49bde2fb535512c358e6a486c104afdfbea26d21
tayeful912 <kazitayefulislam12@gmail.com>
2022-05-23 19:43:32 +0600

    insert item api created

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require("express");
-const cors = require("cors")
+const cors = require("cors");
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const { async } = require("@firebase/util");
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,6 +13,62 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
+// console.log(process.env)
+// database
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@focustools0.q3lyf.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+async function run() {
+    try {
+
+
+        await client.connect()
+
+        const itemCollection = client.db("FocusTools").collection("items");
+
+
+
+
+        //insert item api
+
+
+        app.put('/additem', async (req, res) => {
+
+            const item = req.body;
+            console.log(item)
+            const result = await itemCollection.insertOne(item)
+            res.send(result)
+
+
+        })
+
+
+
+
+    }
+
+    finally {
+
+
+    }
+}
+
+run().catch(console.dir)
+
+
+
+
+
+//check api
 app.get('/api', (req, res) => {
     res.send('Server api')
 })
