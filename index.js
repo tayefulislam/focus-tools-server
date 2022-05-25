bfb3de00208fdb5baba04f3a06f215266c7616da
tayeful912 <kazitayefulislam12@gmail.com>
2022-05-25 23:04:28 +0600

    delete item api created

--- index.js
+++ index.js
@@ -187,6 +187,15 @@ async function run() {
             res.send(result)
         })
 
+        // delete item (admin)
+        app.post('/delete/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await itemCollection.deleteOne(query);
+            res.send(result);
+
+        })
+
 
         // get singel order by id
 
