aa9ebbf60e521b5d4c43f19206c291a6a694c9c8
tayeful912 <kazitayefulislam12@gmail.com>
2022-05-30 15:41:33 +0600

    order item delete bug fix

--- index.js
+++ index.js
@@ -284,8 +284,8 @@ async function run() {
             res.send(result)
         })
 
-
-        app.post('/order/delete/:id', verifyJWT, verifyAdmin, async (req, res) => {
+        // user 
+        app.post('/order/delete/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await orderCollection.deleteOne(query);
